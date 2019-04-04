76976a98cb026b5a6200a073e41941e96abf70d8
huruji <huruji3@foxmail.com>
2019-04-04 10:50:04 +0800

    feat: modify message

--- src/plugin.js
+++ src/plugin.js
@@ -1,11 +1,29 @@
+const chalk = require('chalk')
+
 module.exports = class ICU {
   constructor(opts) {
-    this.opts = this.opts
+    this.opts = opts
   }
+
+  /* eslint-disable */
   apply(compiler) {
     compiler.hooks.entryOption.tap('webpack-plugin-996icu', () => {
-      console.error('996icu')
-      process.exit()
+      let msg = ''
+      const day = (new Date()).getDay()
+      const hour = (new Date()).getHours()
+
+      if (day > 4) {
+        msg = 'webpack 拒绝在 周末 加班编译打包'
+      } else if (hour >= 18) {
+        msg = 'webpack 拒绝在每天 18:00 之后加班编译打包'
+      } else if (hour < 9) {
+        msg = 'webpack 拒绝在每天 9:00 之前加班编译打包'
+      }
+
+      if (msg) {
+        chalk.red(msg)
+        process.exit()
+      }
     })
   }
 }
